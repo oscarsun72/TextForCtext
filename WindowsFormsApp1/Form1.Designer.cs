30d7a4a4eeed285e20578cc9027559acd7d541c0
Oscar Sun（孫守真字任真） <oscarsun72@hotmail.com>
2021-12-10 17:56:20 +0800

    ok

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -29,13 +29,80 @@ namespace WindowsFormsApp1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體-ExtB", 23.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(-1, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(459, 388);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-1, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
+            this.button1.Text = "分行分段";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(296, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(162, 22);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TabStop = false;
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(462, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.TopMost = true;
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +17,226 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {//據第一行長度來分行分段
+            bool noteFlg = false;
+            string x = textBox1.Text; 
+            if(x=="")Clipboard.GetText();
+            const string omitStr = "{}<p>《》〈〉：，。「」『』　0123456789-‧·\r\n";
+            int wordCntr = 0; int noteCtr = 0;
+            StringInfo mystrInof = new StringInfo(x);
+            if (x.IndexOf(Environment.NewLine) == -1)
+            {
+                MessageBox.Show("請先於第一行分段");
+                return;
+            }
+            if ((x.IndexOf("{") == -1 && x.IndexOf("}") > -1) || x.IndexOf("}") < x.IndexOf("{"))
+            {
+                noteFlg = true;
+            }
+            string resltTxt = x.Substring(0, x.IndexOf("\r\n"));
+            x = x.Replace("\r\n", "");
+            TextElementEnumerator mystrEnum = StringInfo.GetTextElementEnumerator(resltTxt);
+            while (mystrEnum.MoveNext())
+            {
+                string mystr = mystrEnum.Current.ToString();
+                if (mystr == "{") noteFlg = true;
+                if (mystr == "}")
+                {
+                    noteFlg = false;
+                    if (noteCtr % 2 == 1) noteCtr++;
+                }
+                if (noteFlg)
+                {
+                    if (omitStr.IndexOf(mystr) == -1)
+                    {
+                        noteCtr++;
+                    }
+                }
+                else
+                {
+                    if (omitStr.IndexOf(mystr) == -1)
+                    {
+                        wordCntr++;
+
+                    }
+
+                }
+            }
+            int lineLen = 0;
+            if (wordCntr == 0 && noteFlg)//純注文
+                lineLen = noteCtr;
+            else
+                lineLen = wordCntr + noteCtr / 2;//wordCntr+((int)Math.Round(noteCtr/2.0));
+            resltTxt = ""; wordCntr = 0; noteCtr = 0; int noteBrk = 0; int noteBrkCtr = 0; noteFlg = false;
+            if ((x.IndexOf("{") == -1 && x.IndexOf("}") > -1) || x.IndexOf("}") < x.IndexOf("{"))
+            {
+                noteFlg = true;
+            }
+            mystrEnum = StringInfo.GetTextElementEnumerator(x);
+            while (mystrEnum.MoveNext())
+            {
+                string mystr = mystrEnum.Current.ToString();
+                if (mystr == "{")
+                {
+                    noteFlg = true;
+                }
+                if (mystr == "}")
+                {
+                    noteFlg = false;
+                    if (noteCtr % 2 == 1) noteCtr++;
+                }
+                if (noteFlg)
+                {//如果是注文                    
+                    if (omitStr.IndexOf(mystr) == -1)
+                    {
+                        noteCtr++;
+                    }
+
+                }
+                else
+                {//正文                    
+                    if (omitStr.IndexOf(mystr) == -1)
+                        wordCntr++;
+                }
+                resltTxt += mystr;
+
+                //if (wordCntr + Math.Ceiling(noteCtr / 2.0) == lineLen)
+                //if (wordCntr + Math.Round(noteCtr / 2.0) == lineLen)                
+                if (wordCntr + noteCtr / 2 == lineLen)
+                {
+                    if (wordCntr == 0)
+                    {//純注文
+                        StringInfo resltxtinof = new StringInfo(resltTxt);
+                        for (int i = resltxtinof.LengthInTextElements; i > -1; i--)
+                        {
+
+                            if (omitStr.IndexOf(resltxtinof.SubstringByTextElements(i - 1)) == -1)
+                            { noteBrkCtr++; }
+                            if (noteBrkCtr == lineLen)
+                            {
+                                noteBrk = i - 1;
+                                noteBrkCtr = 0;
+                                break;
+                            }
+                        }
+                        resltTxt = resltxtinof.SubstringByTextElements(0, noteBrk)
+                            + System.Environment.NewLine + resltxtinof.SubstringByTextElements(noteBrk);
+                    }
+                    resltTxt += "\r\n";
+                    wordCntr = 0;
+                    noteBrk = 0;
+                    noteCtr = 0;
+                }
+
+
+            }
+            textBox1.Text = resltTxt;
+            //Clipboard.SetText(resltTxt);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "") return;
+            string x = textBox1.Text; int xStart = x.IndexOf(textBox2.Text);
+            if (xStart > 0)
+            {
+                textBox1.Focus();
+                textBox1.Select(xStart, textBox2.Text.Length);
+                textBox1.ScrollToCaret();
+                x = x.Substring(0, xStart + textBox2.Text.Length);
+                Clipboard.SetText(x);
+                //textBox1.Text = textBox1.Text.Substring(xStart + 2);
+            }
+
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            newTextBox1();
+        }
+
+        private void newTextBox1()
+        {
+            if (textBox1.Text == "") return;
+            if (textBox1.SelectedText != "")
+            {
+                textBox2.Text = "";
+                string x = textBox1.Text;
+                x = x.Substring(textBox1.SelectionStart + textBox1.SelectionLength + 2);
+                textBox1.Text = x;
+            }
+            if (textBox1.Text.Substring(0, 2) == Environment.NewLine) textBox1.Text = textBox1.Text.Substring(2);
+            textBox1.SelectionStart = 0;textBox1.SelectionLength = 0;
+            textBox1.ScrollToCaret();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_Click(object sender, EventArgs e)
+        {
+            string x = Clipboard.GetText();
+            if (x.Substring(0, 4) == "http")
+                if (x.IndexOf("ctext.org") > -1)
+                {
+                    textBox3.Text = x;
+                }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.PageDown || e.KeyCode == Keys.PageUp)
+            {
+
+                string url = textBox3.Text;
+                if (url == "") return;
+                int edit = url.IndexOf("&editwiki");
+                int page = 0; string urlSub = url;
+                if (edit > -1)
+                {
+                    urlSub = url.Substring(0, url.IndexOf("&page=") + "&page=".Length);
+                    page = Int32.Parse(
+                        url.Substring(url.IndexOf("&page=") + "&page=".Length,
+                        url.IndexOf("&editwiki=") - (url.IndexOf("&page=") + "&page=".Length)));
+                    if (e.KeyCode == Keys.PageDown)
+                        url = urlSub + (page + 1).ToString() + url.Substring(url.IndexOf("&editwiki="));
+                    if (e.KeyCode == Keys.PageUp)
+                        url = urlSub + (page - 1).ToString() + url.Substring(url.IndexOf("&editwiki="));
+                    newTextBox1();
+                }
+                else
+                {
+                    urlSub = url.Substring(0, url.IndexOf("&page=") + "&page=".Length);
+                    page = Int32.Parse(url.Substring(url.IndexOf("&page=") + "&page=".Length));
+                    if (e.KeyCode == Keys.PageDown)
+                        url = urlSub + (page + 1).ToString();
+                    if (e.KeyCode == Keys.PageUp)
+                        url = urlSub + (page - 1).ToString();
+                }
+                Process.Start(url);
+                textBox3.Text = url;
+            }
         }
     }
 }
