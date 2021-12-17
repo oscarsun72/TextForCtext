a1ee465266a765e37cd2a022ff4728d084a7443e
Oscar Sun（孫守真字任真） <oscarsun72@hotmail.com>
2021-12-17 12:02:51 +0800

    ok

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -250,9 +250,11 @@ namespace WindowsFormsApp1
             int s = textBox1.SelectionStart, l = textBox1.SelectionLength;
             string xCopy = x.Substring(0, s + l).Replace("}" + Environment.NewLine + "}", "}}" + Environment.NewLine)
                 .Replace(Environment.NewLine + "》", "》" + Environment.NewLine)
-                .Replace(Environment.NewLine + "〉", "〉" + Environment.NewLine);
+                .Replace(Environment.NewLine + "〉", "〉" + Environment.NewLine
+                .Replace(Environment.NewLine + "}}", "}}" + Environment.NewLine)
+                .Replace("{{" + Environment.NewLine, Environment.NewLine + "{{"));
             Clipboard.SetText(xCopy);
-            if (xCopy.IndexOf(" ")>-1||xCopy.IndexOfAny("�".ToCharArray())>-1)
+            if (xCopy.IndexOf(" ") > -1 || xCopy.IndexOfAny("�".ToCharArray()) > -1)
             {//  「�」甚特別，indexof會失效，明明沒有，而傳回 0 //https://docs.microsoft.com/zh-tw/dotnet/csharp/how-to/compare-strings
              //  //https://docs.microsoft.com/zh-tw/dotnet/api/system.string.compare?view=net-6.0
                 SystemSounds.Exclamation.Play();//文本有缺字警告
